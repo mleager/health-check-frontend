1051e5da94defc7a6573011f29cef894210f3b5e
mark <markleager92@gmail.com>
2025-06-25 00:16:41 -0400

    add dns and cert validation to dns.tf

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,7 +2,38 @@ data "aws_route53_zone" "primary" {
   name = var.route53_zone_name
 }
 
-resource "aws_route53_record" "record" {
+resource "aws_acm_certificate" "certificate" {
+  domain_name       = var.route53_zone_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.primary.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert_validation_waiter" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "record_to_cloudfront" {
   zone_id = data.aws_route53_zone.primary.id
   name    = var.route53_zone_name
   type    = "A"
@@ -12,10 +43,5 @@ resource "aws_route53_record" "record" {
     zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = true
   }
-}
-
-resource "aws_acm_certificate" "certificate" {
-  domain_name       = var.route53_zone_name
-  validation_method = "DNS"
 }
 
