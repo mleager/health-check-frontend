2b0bb406b8cf687e2252a704c87acc743e289cbb
mark <markleager92@gmail.com>
2025-06-24 23:35:44 -0400

    fix type in route53_record resource

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,14 +2,15 @@ data "aws_route53_zone" "primary" {
   name = var.route53_zone_name
 }
 
-resource "aws_route53_recourd" "record" {
+resource "aws_route53_record" "record" {
   zone_id = data.aws_route53_zone.primary.id
-  name    = var.route53_zone
+  name    = var.route53_zone_name
   type    = "A"
 
   alias {
-    name    = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = true
   }
 }
 
